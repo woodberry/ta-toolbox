1f1b2ecadcd42496e2e59642cf011c9662d71870
woodberry <danh.nguyen@woodberry.net.au>
2015-02-19 15:13:18 +1030

    Fix MarketCapitalization formula

--- src/main/java/au/net/woodberry/ta/toolbox/enums/MarketCapitalization.java
+++ src/main/java/au/net/woodberry/ta/toolbox/enums/MarketCapitalization.java
@@ -13,25 +13,24 @@ public enum MarketCapitalization {
     NANO_CAP,
     UNKNOWN;
     
+    private static final TADecimal MEGA_CAP_MINIMUM = TADecimal.valueOf(200000000000.0); // 200 Billion
+    private static final TADecimal LARGE_CAP_MINIMUM = TADecimal.valueOf(10000000000.0); // 10 Billion
+    private static final TADecimal MID_CAP_MINIMUM = TADecimal.valueOf(2000000000.0);    // 2 Billion
+    private static final TADecimal SMALL_CAP_MINIMUM = TADecimal.valueOf(300000000.0);   // 300 Million
+    private static final TADecimal MICRO_CAP_MINIMUM = TADecimal.valueOf(50000000.0);    // 50 million
     
-    private static final TADecimal MEGA_CAP_MINIMUM = TADecimal.valueOf(200000000000.0);
-    private static final TADecimal LARGE_CAP_MINIMUM = TADecimal.valueOf(10000000000.0);
-    private static final TADecimal MID_CAP_MINIMUM = TADecimal.valueOf(2000000000.0);
-    private static final TADecimal SMALL_CAP_MINIMUM = TADecimal.valueOf(300000000.0);
-    private static final TADecimal MICRO_CAP_MINIMUM = TADecimal.valueOf(50000000.0);
-    
-    public static MarketCapitalization valueOf(TADecimal value) {
-        if (value == null || value.isEqual(TADecimal.ZERO)) {
+    public static MarketCapitalization valueOf(TADecimal sharesOutstanding) {
+        if (sharesOutstanding == null || sharesOutstanding.isEqual(TADecimal.ZERO)) {
             return UNKNOWN;
-        } else if (value.isGreaterThanOrEqual(MEGA_CAP_MINIMUM)) {
+        } else if (sharesOutstanding.isGreaterThanOrEqual(MEGA_CAP_MINIMUM)) {
             return MEGA_CAP;
-        } else if (value.isGreaterThanOrEqual(LARGE_CAP_MINIMUM)) {
+        } else if (sharesOutstanding.isGreaterThanOrEqual(LARGE_CAP_MINIMUM)) {
             return LARGE_CAP;
-        } else if (value.isGreaterThanOrEqual(MID_CAP_MINIMUM)) {
+        } else if (sharesOutstanding.isGreaterThanOrEqual(MID_CAP_MINIMUM)) {
             return MID_CAP;
-        } else if (value.isGreaterThanOrEqual(SMALL_CAP_MINIMUM)) {
+        } else if (sharesOutstanding.isGreaterThanOrEqual(SMALL_CAP_MINIMUM)) {
             return SMALL_CAP;
-        } else if (value.isGreaterThanOrEqual(MICRO_CAP_MINIMUM)) {
+        } else if (sharesOutstanding.isGreaterThanOrEqual(MICRO_CAP_MINIMUM)) {
             return MICRO_CAP;
         } else {
             return NANO_CAP;
